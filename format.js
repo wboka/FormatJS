e50cef489967e5676889929fa3a4d317088b13c7
Wayne Boka <wboka@users.noreply.github.com>
2016-10-28 14:16:12 -0400

    Adding string pluralize function

--- format.js
+++ format.js
@@ -3,5 +3,10 @@ var Format = {
 		locale: function(d) {
 			return d.toLocaleString();
 		}			
-	}		
+	},
+	string: {
+		pluralize: function(n, s) {
+			return n.toString() + " " + s + (n === 1 ? "" : (s.charAt(s.length -1).toLowerCase() === "s" ? "es" : "s"));
+		}
+	}
 };
